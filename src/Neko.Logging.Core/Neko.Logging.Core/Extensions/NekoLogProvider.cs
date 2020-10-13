5b55f9aa6f4f9d6055b7cd47a605df0538e693b5
SKYC829 <13677727680@163.com>
2020-10-13 16:38:56 +0800

    Create Logger Provider

---
+++ src/Neko.Logging.Core/Neko.Logging.Core/Extensions/NekoLogProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.Logging
+{
+    /// <summary>
+    /// NekoLog的日志提供器
+    /// </summary>
+    internal class NekoLogProvider : ILoggerProvider
+    {
+        private readonly NekoLogConfiguration _logConfiguration;
+        private readonly ConcurrentDictionary<string, INekoLog> _loggers = new ConcurrentDictionary<string, INekoLog>();
+
+        public NekoLogProvider(NekoLogConfiguration configuration)
+        {
+            _logConfiguration = configuration;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, name => new NekoLogger());
+        }
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+        }
+    }
+}
